1f461d94b3a5041e361d43441694326de728cecf
Joowon <joowon.jang@hotmail.com>
2020-12-28 15:20:12 +0900

    data-type

---
+++ data-type.py
@@ -0,0 +1,28 @@
+#자료형
+
+#숫자 자료형
+print(1)
+
+#문자열 자료형
+print("Solution")
+
+#boolean 자료형
+print(5 > 10)
+print(5 < 10)
+print(not True)
+print(False)
+print(not (5 > 10))
+
+#변수
+animal = "강아지"
+name = "디노"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+
+print("우리집" + animal + "의 이름은" + name + "예요")
+print(name, "는", age, "살이며,", hobby, "를 아주 좋아해요")
+#comma가 들어가면 띄어쓰기 자동으로 들어감
+
+station = "사당"
+print( station + "행 열차가 들어오고 있습니다.")
